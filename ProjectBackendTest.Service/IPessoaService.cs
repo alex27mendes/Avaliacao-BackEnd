29e4476a3ffb4ccbd8038d7f6e1163db7b7e4759
LAPTOP-MM30931E\Alex <alex27.mendes@gmail.com>
2019-05-06 19:01:55 -0300

    commit.

---
+++ ProjectBackendTest.Service/IPessoaService.cs
@@ -0,0 +1,16 @@
+﻿using ProjectBackendTest.Model;
+using ProjectBackendTest.Models.Request;
+using System;
+using System.Collections.Generic;
+
+namespace ProjectBackendTest.Services
+{
+    public interface IPessoaService
+    {
+        PessoaRequest GetPessoa(int Id);
+        List<Pessoa> GetPessoas();
+        bool SalvarPessoa(PessoaRequest pessoaRequest);
+        bool AtualizarPessoa(int Id, PessoaRequest pessoaRequest);
+        bool RemoverPessoa(int Id);
+    }
+}
---
+++ ProjectBackendTest.Service/PessoaService.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ProjectBackendTest.DAL;
+using ProjectBackendTest.Model;
+using ProjectBackendTest.Models.Request;
+
+namespace ProjectBackendTest.Services
+{
+    public class PessoaService : IPessoaService
+    {
+        private readonly IPessoaRepository _pessoaRepository;
+
+        public PessoaService(IPessoaRepository pessoaRepository)
+        {
+            _pessoaRepository = pessoaRepository;
+        }
+        public bool AtualizarPessoa(int Id, PessoaRequest request)
+        {
+            var _pessoa = new Pessoa
+            {
+                Id = Id,
+                Nome = request.Nome,
+                Email = request.Email,
+                Telefone = request.Telefone,
+                DDD = request.DDD
+            };
+            if (request.Endereco != null)
+            {
+                _pessoa.Endereco = new Model.Endereco
+                {
+                    Rua = request.Endereco.Rua,
+                    Numero = request.Endereco.Numero,
+                    Bairro = request.Endereco.Bairro,
+                    Cidade = request.Endereco.Cidade,
+                    Cep = request.Endereco.Cep,
+                    UF = request.Endereco.UF,
+                    Complemento = request.Endereco.Complemento
+
+                };
+            }
+
+            var result = _pessoaRepository.Update(_pessoa);
+            return result;
+        }
+
+        public PessoaRequest GetPessoa(int Id)
+        {
+            var pessoa = _pessoaRepository.Find(Id);
+
+            if (pessoa == null)
+            {
+                return null;
+            }
+            var response = new PessoaRequest
+            {
+                Id = pessoa.Id,
+                Nome = pessoa.Nome,
+                Email = pessoa.Email,
+                Telefone = pessoa.Telefone,
+                DDD = pessoa.DDD,
+                Endereco = new EnderecoRequest
+                {
+                    Id = pessoa.Endereco.Id,
+                    Rua = pessoa.Endereco.Rua,
+                    Numero = pessoa.Endereco.Numero,
+                    Bairro = pessoa.Endereco.Bairro,
+                    Cidade = pessoa.Endereco.Cidade,
+                    Cep = pessoa.Endereco.Cep,
+                    UF = pessoa.Endereco.UF,
+                    Complemento = pessoa.Endereco.Complemento
+                }
+
+            };
+            return response;
+        }
+
+        public List<Pessoa> GetPessoas()
+        {
+            var result = _pessoaRepository.GetAll();
+            return result;
+        }
+
+        public bool RemoverPessoa(int Id)
+        {
+            var pessoa = _pessoaRepository.Find(Id);
+            if (pessoa == null)
+            {
+                return false;
+            }
+            var result = _pessoaRepository.Remove(c => c == pessoa);
+            return result;
+        }
+
+        public bool SalvarPessoa(PessoaRequest pessoaRequest)
+        {
+            var _pessoa = new Pessoa
+            {
+                Nome = pessoaRequest.Nome,
+                Email = pessoaRequest.Email,
+                Telefone = pessoaRequest.Telefone,
+                DDD = pessoaRequest.DDD
+            };
+            if (pessoaRequest.Endereco != null)
+            {
+                _pessoa.Endereco = new Model.Endereco
+                {
+                    Rua = pessoaRequest.Endereco.Rua,
+                    Numero = pessoaRequest.Endereco.Numero,
+                    Bairro = pessoaRequest.Endereco.Bairro,
+                    Cidade = pessoaRequest.Endereco.Cidade,
+                    Cep = pessoaRequest.Endereco.Cep,
+                    UF = pessoaRequest.Endereco.UF,
+                    Complemento = pessoaRequest.Endereco.Complemento
+                };
+            }
+            return _pessoaRepository.Save(_pessoa);
+
+        }
+    }
+}
